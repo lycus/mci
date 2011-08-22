049469c278a6bb0b3b17d47a8424cb86994369e2
XTZGZoReX <xtzgzorex@gmail.com>
2011-08-22 14:58:10 +0200

    Make Tuple(...) a struct.

--- mci.core/tuple.d
+++ mci.core/tuple.d
@@ -1,6 +1,6 @@
 module mci.core.tuple;
 
-public final class Tuple(X)
+public final struct Tuple(X)
 {
     private X _x;
     
@@ -15,7 +15,7 @@ public final class Tuple(X)
     }
 }
 
-public final class Tuple(X, Y)
+public final struct Tuple(X, Y)
 {
     private X _x;
     private Y _y;
@@ -37,7 +37,7 @@ public final class Tuple(X, Y)
     }
 }
 
-public final class Tuple(X, Y, Z)
+public final struct Tuple(X, Y, Z)
 {
     private X _x;
     private Y _y;
