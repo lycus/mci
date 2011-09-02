590225d0b2e46d505ac6546e3bbf6c0349bb8dc9
Alex Rønne Petersen <xtzgzorex@gmail.com>
2011-09-02 17:13:17 +0200

    Added LexerException.

--- mci/assembler/exception.d
+++ mci/assembler/exception.d
@@ -30,3 +30,32 @@ public class AssemblerException : CompilerException
         super(msg, next, file, line);
     }
 }
+
+public class LexerException : AssemblerException
+{
+    public this(string msg, string file = __FILE__, size_t line = __LINE__)
+    in
+    {
+        assert(msg);
+        assert(file);
+        assert(line);
+    }
+    body
+    {
+        super(msg, file, line);
+    }
+    
+    public this(string msg, Throwable next, string file = __FILE__,
+                size_t line = __LINE__)
+    in
+    {
+        assert(msg);
+        assert(next);
+        assert(file);
+        assert(line);
+    }
+    body
+    {
+        super(msg, next, file, line);
+    }
+}
