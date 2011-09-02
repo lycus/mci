f965af736816b5a01a98a7c1b3e87e7e91150043
Alex Rønne Petersen <xtzgzorex@gmail.com>
2011-09-02 09:00:51 +0200

    Some stub classes.

--- mci/core/tree/expressions.d
+++ mci/core/tree/expressions.d
@@ -1,1 +1,19 @@
 module mci.core.tree.expressions;
+
+import mci.core.tree.base;
+
+public abstract class ExpressionNode : TreeNode
+{
+}
+
+public abstract class UnaryExpressionNode : ExpressionNode
+{
+}
+
+public abstract class BinaryExpressionNode : ExpressionNode
+{
+}
+
+public abstract class TernaryExpressionNode : ExpressionNode
+{
+}
