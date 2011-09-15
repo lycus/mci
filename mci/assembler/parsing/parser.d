679f07544ea34e539d218188ed2b126f9752cac1
Alex Rønne Petersen <xtzgzorex@gmail.com>
2011-09-15 11:52:53 +0200

    Some parser work.

--- mci/assembler/parsing/parser.d
+++ mci/assembler/parsing/parser.d
@@ -1,6 +1,9 @@
 module mci.assembler.parsing.parser;
 
-import mci.core.container,
+import std.conv,
+       mci.core.container,
+       mci.core.diagnostics.debugging,
+       mci.assembler.exception,
        mci.assembler.parsing.ast,
        mci.assembler.parsing.tokens;
 
@@ -36,5 +39,55 @@ public final class Parser
     body
     {
         _stream = stream;
+    }
+
+    private Token next()
+    {
+        auto token = _stream.moveNext();
+
+        // EOF not allowed.
+        if (token.type == TokenType.end)
+            errorGot("any token", token.location, "end of file");
+
+        return token;
+    }
+
+    private Token nextEof()
+    {
+        return _stream.moveNext();
+    }
+
+    private static void error(string expected, SourceLocation location)
+    {
+        throw new ParserException("Expected " ~ expected ~ ".", location);
+    }
+
+    private static void errorGot(T)(string expected, SourceLocation location, T got)
+    {
+        throw new ParserException("Expected " ~ expected ~ ", but got " ~ to!string(got) ~ ".", location);
+    }
+
+    public CompilationUnit parse()
+    {
+        _stream.reset();
+        _stream.moveNext(); // Skip begin token.
+
+        auto ast = new NoNullList!DeclarationNode();
+
+        Token token;
+
+        while ((token = nextEof()).type != TokenType.end)
+        {
+            if (token.type == TokenType.type)
+            {
+            }
+            else if (token.type == TokenType.method)
+            {
+            }
+            else
+                errorGot("'type' or 'function'", token.location, token.value);
+        }
+
+        return new CompilationUnit(ast);
     }
 }
