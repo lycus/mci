7d516b7eb530bc9a9a4e433c02adbb92468934d0
Alex Rønne Petersen <xtzgzorex@gmail.com>
2011-09-15 11:06:09 +0200

    Added parser skeleton.

--- mci/assembler/parsing/parser.d
+++ mci/assembler/parsing/parser.d
@@ -1,1 +1,40 @@
 module mci.assembler.parsing.parser;
+
+import mci.core.container,
+       mci.assembler.parsing.ast,
+       mci.assembler.parsing.tokens;
+
+public final class CompilationUnit
+{
+    private NoNullList!DeclarationNode _nodes;
+
+    public this(NoNullList!DeclarationNode nodes)
+    in
+    {
+        assert(nodes);
+    }
+    body
+    {
+        _nodes = nodes;
+    }
+
+    @property public Countable!DeclarationNode nodes()
+    {
+        return _nodes;
+    }
+}
+
+public final class Parser
+{
+    private TokenStream _stream;
+
+    public this(TokenStream stream)
+    in
+    {
+        assert(stream);
+    }
+    body
+    {
+        _stream = stream;
+    }
+}
