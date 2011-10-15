8433e6285fa080fd2184d15d714cb3ba7a3f81ba
alexrp <xtzgzorex@gmail.com>
2011-10-15 16:27:59 +0200

    Add some utility functions to the parser.

--- mci/assembler/parsing/parser.d
+++ mci/assembler/parsing/parser.d
@@ -1,8 +1,11 @@
 module mci.assembler.parsing.parser;
 
 import std.conv,
+       std.string,
        mci.core.container,
        mci.core.diagnostics.debugging,
+       mci.core.nullable,
+       mci.core.typing.types,
        mci.assembler.exception,
        mci.assembler.parsing.ast,
        mci.assembler.parsing.tokens;
@@ -41,7 +44,25 @@ public final class Parser
         _stream = stream;
     }
 
+    private Token peek()
+    in
+    {
+        assert(!_stream.done);
+    }
+    body
+    {
+        if (_stream.next.type == TokenType.end)
+            errorGot("any token", _stream.current.location, "end of file");
+
+        return _stream.next;
+    }
+
     private Token next()
+    in
+    {
+        assert(!_stream.done);
+    }
+    body
     {
         auto token = _stream.moveNext();
 
@@ -53,18 +74,41 @@ public final class Parser
     }
 
     private Token nextEof()
+    in
+    {
+        assert(!_stream.done);
+    }
+    body
     {
         return _stream.moveNext();
     }
 
-    private static void error(string expected, SourceLocation location)
+    private void consume(string expect)
+    in
+    {
+        assert(!_stream.done);
+    }
+    body
+    {
+        auto next = next();
+
+        if (next.value != expect)
+            errorGot("'" ~ expect ~ "'", next.location, next.value);
+    }
+
+    private static void error(string error, SourceLocation location)
+    {
+        throw new ParserException(error ~ ".", location);
+    }
+
+    private static void errorExpected(string expected, SourceLocation location)
     {
         throw new ParserException("Expected " ~ expected ~ ".", location);
     }
 
     private static void errorGot(T)(string expected, SourceLocation location, T got)
     {
-        throw new ParserException("Expected " ~ expected ~ ", but got " ~ to!string(got) ~ ".", location);
+        throw new ParserException("Expected " ~ expected ~ ", but got '" ~ to!string(got) ~ "'.", location);
     }
 
     public CompilationUnit parse()
