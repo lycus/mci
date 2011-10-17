536cf7a1a1ce77cd306ffe81b5dac7c5f5da6419
alexrp <xtzgzorex@gmail.com>
2011-10-17 20:48:20 +0200

    Update all.d in mci.assembler.

--- mci/assembler/all.d
+++ mci/assembler/all.d
@@ -1,5 +1,6 @@
 module mci.assembler.all;
 
+public import mci.assembler.parsing.ast;
 public import mci.assembler.parsing.lexer;
 public import mci.assembler.parsing.parser;
 public import mci.assembler.parsing.tokens;
