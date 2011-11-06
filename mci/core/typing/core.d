338c781d817d861837bda675236dc328444b0dbd
alexrp <xtzgzorex@gmail.com>
2011-11-07 00:25:59 +0100

    Introduce CoreType class that all core types derive from.

--- mci/core/typing/core.d
+++ mci/core/typing/core.d
@@ -2,9 +2,13 @@ module mci.core.typing.core;
 
 import mci.core.typing.types;
 
-private mixin template CoreType(string type, string name)
+public abstract class CoreType : Type
 {
-    mixin("public final class " ~ type ~ "Type : Type" ~
+}
+
+private mixin template DefineCoreType(string type, string name)
+{
+    mixin("public final class " ~ type ~ "Type : CoreType" ~
           "{" ~
           "    private static " ~ type ~ "Type _instance;" ~
           "" ~
@@ -29,17 +33,17 @@ private mixin template CoreType(string type, string name)
           "}");
 }
 
-mixin CoreType!("Unit", "unit");
-mixin CoreType!("Int8", "int8");
-mixin CoreType!("UInt8", "uint8");
-mixin CoreType!("Int16", "int16");
-mixin CoreType!("UInt16", "uint16");
-mixin CoreType!("Int32", "int32");
-mixin CoreType!("UInt32", "uint32");
-mixin CoreType!("Int64", "int64");
-mixin CoreType!("UInt64", "uint64");
-mixin CoreType!("NativeInt", "int");
-mixin CoreType!("NativeUInt", "uint");
-mixin CoreType!("Float32", "float32");
-mixin CoreType!("Float64", "float64");
-mixin CoreType!("NativeFloat", "float");
+mixin DefineCoreType!("Unit", "unit");
+mixin DefineCoreType!("Int8", "int8");
+mixin DefineCoreType!("UInt8", "uint8");
+mixin DefineCoreType!("Int16", "int16");
+mixin DefineCoreType!("UInt16", "uint16");
+mixin DefineCoreType!("Int32", "int32");
+mixin DefineCoreType!("UInt32", "uint32");
+mixin DefineCoreType!("Int64", "int64");
+mixin DefineCoreType!("UInt64", "uint64");
+mixin DefineCoreType!("NativeInt", "int");
+mixin DefineCoreType!("NativeUInt", "uint");
+mixin DefineCoreType!("Float32", "float32");
+mixin DefineCoreType!("Float64", "float64");
+mixin DefineCoreType!("NativeFloat", "float");
