dd5f64cf65c82a8a05a47df5c8ec021ddff06f72
alexrp <xtzgzorex@gmail.com>
2011-11-09 02:33:32 +0100

    Return 1 in mci.tester when no tests are compiled in.

--- mci/tester/main.d
+++ mci/tester/main.d
@@ -4,13 +4,14 @@ import std.stdio,
        mci.assembler.all,
        mci.core.all;
 
-private void main()
+private int main()
 {
     // This main function implicitly triggers unit tests
     // of all loaded modules. This is its sole purpose.
     version (unittest)
     {
         writeln("All unit tests passed!");
+        return 0;
     }
     else
     {
@@ -18,5 +19,6 @@ private void main()
         // happen, but just in case someone compiles it
         // in an awkward way...
         writeln("Not compiled with unit tests; no tests run.");
+        return 1;
     }
 }
