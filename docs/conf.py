c88d253ed9b83c6f03fad43cdb0597b3a611d905
alexrp <xtzgzorex@gmail.com>
2011-11-12 00:11:23 +0100

    Fix some titles in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ extensions = ['sphinx.ext.todo']
 templates_path = ['_templates']
 exclude_patterns = ['_build']
 
-project = u'MCI'
+project = u'Managed Compiler Infrastructure'
 copyright = u'The Lycus Foundation'
 
 version = '1.0'
@@ -22,16 +22,16 @@ html_static_path = ['_static']
 htmlhelp_basename = 'MCIdoc'
 
 latex_documents = [
-  ('index', 'MCI.tex', u'MCI Documentation',
+  ('index', 'MCI.tex', u'Managed Compiler Infrastructure',
    u'The Lycus Foundation', 'manual'),
 ]
 
 man_pages = [
-    ('index', 'mci', u'MCI Documentation',
+    ('index', 'mci', u'Managed Compiler Infrastructure',
      [u'The Lycus Foundation'], 1)
 ]
 
-epub_title = u'MCI'
+epub_title = u'Managed Compiler Infrastructure'
 epub_author = u'The Lycus Foundation'
 epub_publisher = u'The Lycus Foundation'
 epub_copyright = u'The Lycus Foundation'
