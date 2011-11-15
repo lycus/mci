51815f39d5e7221d94a6e6cc422e756bb5557075
alexrp <xtzgzorex@gmail.com>
2011-11-15 11:24:19 +0100

    Rework config.d to use enums.
    Also update it to reflect the latest table of version identifiers as
    published on d-p-l.org.

--- mci/core/config.d
+++ mci/core/config.d
@@ -2,16 +2,96 @@ module mci.core.config;
 
 // This file figures out what kind of environment we're running
 // in and sets appropriate constants. Reliance on these constants
-// should, however, be avoided if possible.
+// should be avoided if possible.
+
+public enum Compiler : ubyte
+{
+    unknown,
+    dmd,
+    gdc,
+    ldc,
+    sdc,
+}
+
+public enum Architecture : ubyte
+{
+    x86,
+    arm,
+    ppc,
+    ia64,
+    mips,
+    sparc,
+    s390,
+    hppa,
+    sh,
+    alpha,
+}
+
+public enum Endianness : ubyte
+{
+    littleEndian,
+    bigEndian,
+}
+
+public enum OperatingSystem : ubyte
+{
+    windows,
+    linux,
+    osx,
+    bsd,
+    freebsd,
+    openbsd,
+    solaris,
+    aix,
+    skyos,
+    hurd,
+}
+
+public enum EmulationLayer : ubyte
+{
+    none,
+    cygwin,
+    mingw,
+}
 
 pragma(msg, "------------------------------------------------------------");
 pragma(msg, "Managed Compiler Infrastructure configuration information:");
 
-version (D_NET)
+version (DigitalMars)
+{
+    pragma(msg, "- Compiler: Digital Mars D (DMD)");
+
+    public enum Compiler compiler = Compiler.dmd;
+}
+else version (GNU)
+{
+    pragma(msg, "- Compiler: GNU D Compiler (GDC)");
+
+    public enum Compiler compiler = Compiler.gdc;
+}
+else version (LDC)
+{
+    pragma(msg, "- Compiler: LLVM D Compiler (LDC)");
+
+    public enum Compiler compiler = Compiler.ldc;
+}
+else version (SDC)
+{
+    pragma(msg, "- Compiler: Stupid D Compiler (SDC)");
+
+    public enum Compiler compiler = Compiler.sdc;
+}
+else version (D_NET)
 {
     // We cannot run on D.NET because some things like signals
     // will interfere with the managed runtime.
     static assert(false, "D.NET is not supported.");
+}
+else
+{
+    pragma(msg, "- Compiler: Unknown");
+
+    public enum Compiler compiler = Compiler.unknown;
 }
 
 version (D_Version2)
@@ -23,113 +103,154 @@ else
     static assert(false, "Unsupported D language version.");
 }
 
-version (DigitalMars)
+version (D_InlineAsm_X86_64)
 {
-    pragma(msg, "- Compiler: Digital Mars D (DMD)");
-
-    public enum bool isDMD = true;
-    public enum bool isGDC = false;
-    public enum bool isLDC = false;
-    public enum bool isSDC = false;
+    pragma(msg, "- Inline Assembly: x86-64");
 }
-else version (GNU)
+else version (D_InlineAsm_X86)
 {
-    pragma(msg, "- Compiler: GNU D Compiler (GDC)");
-
-    public enum bool isDMD = false;
-    public enum bool isGDC = true;
-    public enum bool isLDC = false;
-    public enum bool isSDC = false;
-}
-else version (LDC)
-{
-    pragma(msg, "- Compiler: LLVM D Compiler (LDC)");
-
-    public enum bool isDMD = false;
-    public enum bool isGDC = false;
-    public enum bool isLDC = true;
-    public enum bool isSDC = false;
-}
-else version (SDC)
-{
-    pragma(msg, "- Compiler: Stupid D Compiler (SDC)");
-
-    public enum bool isDMD = false;
-    public enum bool isGDC = false;
-    public enum bool isLDC = false;
-    public enum bool isSDC = true;
+    pragma(msg, "- Inline Assembly: x86");
 }
 else
 {
-    pragma(msg, "- Compiler: Unknown");
-
-    public enum bool isDMD = false;
-    public enum bool isGDC = false;
-    public enum bool isLDC = false;
-    public enum bool isSDC = false;
+    static assert(false, "Inline assembly not available.");
 }
 
 version (X86)
 {
-    pragma(msg, "- Architecture: x86");
+    pragma(msg, "- Architecture: x86 (32-bit)");
 
-    public enum bool is32Bit = true;
-    public enum bool is64Bit = false;
+    public enum Architecture architecture = Architecture.x86;
 }
 else version (X86_64)
 {
-    pragma(msg, "- Architecture: x86-64");
+    pragma(msg, "- Architecture: x86 (64-bit)");
 
-    public enum bool is32Bit = false;
-    public enum bool is64Bit = true;
+    public enum Architecture architecture = Architecture.x86;
+}
+else version (ARM)
+{
+    pragma(msg, "- Architecture: ARM (32-bit)");
+
+    public enum Architecture architecture = Architecture.arm;
+}
+else version (PPC)
+{
+    pragma(msg, "- Architecture: PowerPC (32-bit)");
+
+    public enum Architecture architecture = Architecture.ppc;
+}
+else version (PPC64)
+{
+    pragma(msg, "- Architecture: PowerPC (64-bit)");
+
+    public enum Architecture architecture = Architecture.ppc;
+}
+else version (IA64)
+{
+    pragma(msg, "- Architecture: Itanium (64-bit)");
+
+    public enum Architecture architecture = Architecture.ia64;
+}
+else version (MIPS)
+{
+    pragma(msg, "- Architecture: MIPS (32-bit)");
+
+    public enum Architecture architecture = Architecture.mips;
+}
+else version (MIPS64)
+{
+    pragma(msg, "- Architecture: MIPS (64-bit)");
+
+    public enum Architecture architecture = Architecture.mips;
+}
+else version (SPARC)
+{
+    pragma(msg, "- Architecture: SPARC (32-bit)");
+
+    public enum Architecture architecture = Architecture.sparc;
+}
+else version (SPARC64)
+{
+    pragma(msg, "- Architecture: SPARC (64-bit)");
+
+    public enum Architecture architecture = Architecture.sparc;
+}
+else version (S390)
+{
+    pragma(msg, "- Architecture: System/390 (32-bit)");
+
+    public enum Architecture architecture = Architecture.s390;
+}
+else version (S390X)
+{
+    pragma(msg, "- Architecture: System/390 (64-bit)");
+
+    public enum Architecture architecture = Architecture.s390;
+}
+else version (HPPA)
+{
+    pragma(msg, "- Architecture: PA-RISC (32-bit)");
+
+    public enum Architecture architecture = Architecture.hppa;
+}
+else version (HPPA64)
+{
+    pragma(msg, "- Architecture: PA-RISC (64-bit)");
+
+    public enum Architecture architecture = Architecture.hppa;
+}
+else version (SH)
+{
+    pragma(msg, "- Architecture: SuperH (32-bit)");
+
+    public enum Architecture architecture = Architecture.sh;
+}
+else version (SH64)
+{
+    pragma(msg, "- Architecture: SuperH (64-bit)");
+
+    public enum Architecture architecture = Architecture.sh;
+}
+else version (Alpha)
+{
+    pragma(msg, "- Architecture: Alpha (64-bit)");
+
+    public enum Architecture architecture = Architecture.alpha;
 }
 else
 {
     static assert(false, "Processor architecture could not be determined.");
 }
 
+version (D_LP64)
+{
+    pragma(msg, "- Pointer Length: 64-bit");
+
+    public enum bool is32Bit = false;
+}
+else
+{
+    pragma(msg, "- Pointer Length: 32-bit");
+
+    public enum bool is32Bit = true;
+}
+
 version (LittleEndian)
 {
     pragma(msg, "- Byte Order: Little Endian (LE)");
 
-    public enum bool isLittleEndian = true;
-    public enum bool isBigEndian = false;
+    public enum Endianness endianness = Endianness.littleEndian;
 }
 else version (BigEndian)
 {
     pragma(msg, "- Byte Order: Big Endian (BE)");
 
-    public enum bool isLittleEndian = false;
-    public enum bool isBigEndian = true;
+    public enum Endianness endianness = Endianness.bigEndian;
 }
 else
 {
     static assert(false, "Endianness could not be determined.");
-}
-
-version (D_InlineAsm_X86_64)
-{
-    pragma(msg, "- Inline Assembly: x86-64 & x86");
-
-    public enum bool hasAsm = true;
-    public enum bool hasX86Asm = true;
-    public enum bool hasX64Asm = true;
-}
-else version (D_InlineAsm_X86)
-{
-    pragma(msg, "- Inline Assembly: x86");
-
-    public enum bool hasAsm = true;
-    public enum bool hasX86Asm = true;
-    public enum bool hasX64Asm = false;
-}
-else
-{
-    pragma(msg, "- Inline Assembly: None");
-
-    public enum bool hasAsm = false;
-    public enum bool hasX86Asm = false;
-    public enum bool hasX64Asm = false;
 }
 
 version (Windows)
@@ -144,41 +265,102 @@ version (Windows)
     }
     else
     {
-        static assert(false, "Unknown Windows bitness.");
+        static assert(false, "Unknown Windows bit width.");
     }
 
-    public enum bool isWindows = true;
-    public enum bool isPosix = false;
+    public enum OperatingSystem operatingSystem = OperatingSystem.windows;
 }
 else version (Posix)
 {
     version (FreeBSD)
     {
         pragma(msg, "- Operating System: FreeBSD");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.freebsd;
+    }
+    else version (OpenBSD)
+    {
+        pragma(msg, "- Operating System: OpenBSD");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.openbsd;
+    }
+    else version (BSD)
+    {
+        pragma(msg, "- Operating System: BSD");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.bsd;
+    }
+    else version (AIX)
+    {
+        pragma(msg, "- Operating System: AIX");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.aix;
     }
     else version (Solaris)
     {
         pragma(msg, "- Operating System: Solaris");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.solaris;
     }
-    else version (OSX)
+    else version (SkyOS)
     {
-        pragma(msg, "- Operating System: OS X");
+        pragma(msg, "- Operating System: SkyOS");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.skyos;
+    }
+    else version (Hurd)
+    {
+        pragma(msg, "- Operating System: Hurd");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.hurd;
     }
     else version (linux)
     {
         pragma(msg, "- Operating System: Linux");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.linux;
+    }
+    else version (OSX)
+    {
+        pragma(msg, "- Operating System: OS X");
+
+        public enum OperatingSystem operatingSystem = OperatingSystem.osx;
+    }
+    else version (SysV3)
+    {
+        static assert(false, "System V R3 is not supported.");
+    }
+    else version (SysV4)
+    {
+        static assert(false, "System V R4 is not supported.");
     }
     else
     {
         static assert(false, "Unknown POSIX operating system.");
     }
-
-    public enum bool isWindows = false;
-    public enum bool isPosix = true;
 }
 else
 {
     static assert(false, "Operating system could not be determined.");
+}
+
+version (Cygwin)
+{
+    pragma(msg, "- Emulation Layer: Cygwin");
+
+    public enum EmulationLayer emulationLayer = EmulationLayer.cygwin;
+}
+else version (MinGW)
+{
+    pragma(msg, "- Emulation Layer: MinGW");
+
+    public enum EmulationLayer emulationLayer = EmulationLayer.mingw;
+}
+else
+{
+    pragma(msg, "- Emulation Layer: None");
+
+    public enum EmulationLayer emulationLayer = EmulationLayer.none;
 }
 
 pragma(msg, "------------------------------------------------------------");
