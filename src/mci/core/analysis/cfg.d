d6bfeceb46391038f8120d012f42ce442d379188
alexrp <alex@lycus.org>
2012-02-12 19:14:29 +0100

    Control flow graph module (mci.core.analysis.cfg).

---
+++ src/mci/core/analysis/cfg.d
@@ -0,0 +1,61 @@
+module mci.core.analysis.cfg;
+
+import std.variant,
+       mci.core.container,
+       mci.core.tuple,
+       mci.core.code.functions;
+
+public ReadOnlyIndexable!BasicBlock getBranches(BasicBlock block)
+in
+{
+    assert(block);
+}
+out (result)
+{
+    assert(result);
+}
+body
+{
+    auto term = last(block.instructions);
+
+    if (auto bb = term.operand.peek!BasicBlock())
+        return toReadOnlyIndexable(*bb);
+    else if (auto tup = term.operand.peek!(Tuple!(BasicBlock, BasicBlock))())
+        return toReadOnlyIndexable(tup.x, tup.y);
+
+    return new List!BasicBlock();
+}
+
+public bool isReachableFrom(BasicBlock toBlock, BasicBlock fromBlock)
+in
+{
+    assert(toBlock);
+    assert(fromBlock);
+}
+body
+{
+    auto queue = new ArrayQueue!BasicBlock();
+    auto set = new HashSet!BasicBlock();
+
+    queue.enqueue(fromBlock);
+
+    BasicBlock current;
+
+    while (!queue.empty)
+    {
+        current = queue.dequeue();
+
+        foreach (br; getBranches(current))
+        {
+            if (br is toBlock)
+                return true;
+
+            if (!set.add(br))
+                continue;
+
+            queue.enqueue(br);
+        }
+    }
+
+    return false;
+}
