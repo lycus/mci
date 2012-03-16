c0c17a8d504c65b13e839b0bf211374a6f3600d1
alexrp <alex@lycus.org>
2012-03-16 11:26:55 +0100

    Some helper code for the tree model.

--- src/mci/core/tree/base.d
+++ src/mci/core/tree/base.d
@@ -4,9 +4,28 @@ import mci.core.container,
        mci.core.code.instructions,
        mci.core.typing.types;
 
+private __gshared List!TreeNode _emptyNodes;
+
+shared static this()
+{
+    _emptyNodes = new typeof(_emptyNodes)();
+}
+
 public abstract class TreeNode
 {
+    private TreeNode _parent;
+
     @property public abstract Type type();
+
+    @property public final TreeNode parent()
+    {
+        return _parent;
+    }
+
+    @property private final void parent(TreeNode parent)
+    {
+        _parent = parent;
+    }
 
     @property public ReadOnlyIndexable!TreeNode children()
     out (result)
@@ -15,12 +34,95 @@ public abstract class TreeNode
     }
     body
     {
-        return new List!TreeNode();
+        return _emptyNodes;
     }
 
     public TreeNode reduce()
     {
         // No reduction possible by default.
         return null;
+    }
+}
+
+public final class ChildNodeContainer(T : TreeNode) : NoNullList!T
+{
+    private TreeNode _node;
+
+    invariant()
+    {
+        assert(_node);
+    }
+
+    public this(TreeNode node)
+    in
+    {
+        assert(node);
+    }
+    body
+    {
+        _node = node;
+    }
+
+    public override ChildNodeContainer!T opSlice()
+    {
+        return duplicate();
+    }
+
+    public override ChildNodeContainer!T opSlice(size_t x, size_t y)
+    {
+        auto list = new ChildNodeContainer!T();
+
+        for (auto i = x; i < y; i++)
+            list.add(this[i]);
+
+        return list;
+    }
+
+    public override ChildNodeContainer!T opCat(Iterable!T rhs)
+    {
+        auto list = duplicate();
+
+        foreach (item; rhs)
+            list.add(item);
+
+        return list;
+    }
+
+    public override ChildNodeContainer!T opCatAssign(Iterable!T rhs)
+    {
+        foreach (item; rhs)
+            add(item);
+
+        return this;
+    }
+
+    public override ChildNodeContainer!T duplicate()
+    {
+        auto l = new ChildNodeContainer!T();
+
+        foreach (item; this)
+            l.add(item);
+
+        return l;
+    }
+
+    protected override void onAdd(T item)
+    {
+        super.onAdd(item);
+
+        item.parent = _node;
+    }
+
+    protected override void onRemove(T item)
+    {
+        super.onRemove(item);
+
+        item.parent = null;
+    }
+
+    protected override void onClear()
+    {
+        foreach (item; this)
+            item.parent = null;
     }
 }
