b12db662e51e1fbfa2d5ba4052108816c12ebd17
alexrp <alex@lycus.org>
2012-05-01 22:42:36 +0200

    Add StackTrace and StackFrame classes.

--- src/mci/vm/exception.d
+++ src/mci/vm/exception.d
@@ -2,23 +2,24 @@ module mci.vm.exception;
 
 import mci.core.exception,
        mci.core.code.instructions,
-       mci.vm.execution;
+       mci.vm.execution,
+       mci.vm.trace;
 
 public class ExecutionException : CompilerException
 {
-    private Instruction _instruction;
+    private StackTrace _trace;
     private RuntimeValue _exception;
 
     invariant()
     {
-        assert(_instruction);
+        assert(_trace);
         assert(_exception);
     }
 
-    public this(Instruction instruction, RuntimeValue exception, string msg, string file = __FILE__, size_t line = __LINE__)
+    public this(StackTrace trace, RuntimeValue exception, string msg, string file = __FILE__, size_t line = __LINE__)
     in
     {
-        assert(instruction);
+        assert(trace);
         assert(exception);
         assert(msg);
         assert(file);
@@ -28,14 +29,14 @@ public class ExecutionException : CompilerException
     {
         super(msg, file, line);
 
-        _instruction = instruction;
+        _trace = trace;
         _exception = exception;
     }
 
-    public this(Instruction instruction, RuntimeValue exception, string msg, Throwable next, string file = __FILE__, size_t line = __LINE__)
+    public this(StackTrace trace, RuntimeValue exception, string msg, Throwable next, string file = __FILE__, size_t line = __LINE__)
     in
     {
-        assert(instruction);
+        assert(trace);
         assert(exception);
         assert(msg);
         assert(next);
@@ -46,18 +47,18 @@ public class ExecutionException : CompilerException
     {
         super(msg, next, file, line);
 
-        _instruction = instruction;
+        _trace = trace;
         _exception = exception;
     }
 
-    @property public Instruction instruction()
+    @property public StackTrace trace()
     out (result)
     {
         assert(result);
     }
     body
     {
-        return _instruction;
+        return _trace;
     }
 
     @property public RuntimeValue exception()
---
+++ src/mci/vm/trace.d
@@ -0,0 +1,67 @@
+module mci.vm.trace;
+
+import mci.core.container,
+       mci.core.code.instructions;
+
+public final class StackTrace
+{
+    private NoNullList!StackFrame _frames;
+
+    invariant()
+    {
+        assert(_frames);
+        assert(!(cast()_frames).empty);
+    }
+
+    public this(NoNullList!StackFrame frames)
+    in
+    {
+        assert(frames);
+        assert(!frames.empty);
+    }
+    body
+    {
+        _frames = frames;
+    }
+
+    @property public ReadOnlyIndexable!StackFrame frames()
+    out (result)
+    {
+        assert(result);
+        assert(!(cast()result).empty);
+    }
+    body
+    {
+        return _frames;
+    }
+}
+
+public final class StackFrame
+{
+    private Instruction _instruction;
+
+    invariant()
+    {
+        assert(_instruction);
+    }
+
+    public this(Instruction instruction)
+    in
+    {
+        assert(instruction);
+    }
+    body
+    {
+        _instruction = instruction;
+    }
+
+    @property public Instruction instruction()
+    out (result)
+    {
+        assert(result);
+    }
+    body
+    {
+        return _instruction;
+    }
+}
