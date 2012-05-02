517ce8e654841ecaa58aae6a9a3385551f5b240d
alexrp <alex@lycus.org>
2012-05-03 00:47:53 +0200

    Add final Condition type to mci.core.sync.

--- src/mci/core/sync.d
+++ src/mci/core/sync.d
@@ -1,8 +1,22 @@
 module mci.core.sync;
 
-import core.sync.mutex;
+import core.sync.condition,
+       core.sync.mutex;
 
 // Ensure that calls are not virtual.
 public final class Mutex : core.sync.mutex.Mutex
 {
+}
+
+public final class Condition : core.sync.condition.Condition
+{
+    public this(Mutex mutex)
+    in
+    {
+        assert(mutex);
+    }
+    body
+    {
+        super(mutex);
+    }
 }
