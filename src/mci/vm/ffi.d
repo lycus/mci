d0a1020c614d30b7daf3e69a1fec851a2c535061
alexrp <alex@lycus.org>
2012-05-13 21:15:21 +0200

    Add mci.vm.ffi module to load/unload dynamic libraries and get procedure entry points.

---
+++ src/mci/vm/ffi.d
@@ -0,0 +1,71 @@
+module mci.vm.ffi;
+
+import std.string,
+       std.utf,
+       mci.core.config,
+       mci.core.code.functions;
+
+static if (isPOSIX)
+{
+    import core.sys.posix.dlfcn,
+           std.file,
+           std.path;
+}
+else
+{
+    import core.sys.windows.windows;
+}
+
+public alias extern (C) void function() EntryPoint;
+
+public void* openLibrary(string name)
+in
+{
+    assert(name);
+}
+body
+{
+    // FIXME: When 2.060 is released, we should use Runtime.loadLibrary.
+    static if (isPOSIX)
+    {
+        void* lib;
+
+        if (isValidFilename(name))
+            lib = dlopen(toUTFz!(const(char)*)(buildPath(getcwd(), name)), RTLD_LAZY);
+
+        if (!lib)
+            lib = dlopen(toUTFz!(const(char)*)(name), RTLD_LAZY);
+
+        return lib;
+    }
+    else
+        return LoadLibraryW(toUTFz!(const(wchar)*)(name));
+}
+
+public EntryPoint getProcedure(void* library, string name)
+in
+{
+    assert(library);
+    assert(name);
+}
+body
+{
+    static if (isPOSIX)
+        return cast(EntryPoint)dlsym(library, toUTFz!(const(char)*)(name));
+    else
+        return cast(EntryPoint)GetProcAddress(library, name);
+}
+
+public bool closeLibrary(void* library)
+in
+{
+    assert(library);
+}
+body
+{
+    // FIXME: When 2.060 is released, we should use Runtime.unloadLibrary.
+    static if (isPOSIX)
+        return !dlclose(library);
+    else
+        return !!FreeLibrary(library);
+}
