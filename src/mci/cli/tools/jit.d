c94b8aef978b2f87494a73dfc24a20202a66c576
alexrp <alex@lycus.org>
2012-06-22 08:02:58 +0200

    Implement some of the JIT tool.

--- src/mci/cli/tools/jit.d
+++ src/mci/cli/tools/jit.d
@@ -1,8 +1,25 @@
 module mci.cli.tools.jit;
 
-import std.getopt,
+import core.memory,
+       std.getopt,
+       std.path,
        mci.cli.main,
-       mci.cli.tool;
+       mci.cli.tool,
+       mci.core.config,
+       mci.core.container,
+       mci.core.exception,
+       mci.core.io,
+       mci.core.code.modules,
+       mci.jit.engine,
+       mci.vm.intrinsics.declarations,
+       mci.vm.io.exception,
+       mci.vm.io.reader,
+       mci.vm.exception,
+       mci.vm.execution,
+       mci.vm.memory.base,
+       mci.vm.memory.boehm,
+       mci.vm.memory.dgc,
+       mci.vm.memory.libc;
 
 public final class JITTool : Tool
 {
@@ -23,6 +40,120 @@ public final class JITTool : Tool
 
     public ubyte run(string[] args)
     {
+        JITBackEnd backEnd;
+        GarbageCollectorType gcType;
+
+        try
+        {
+            getopt(args,
+                   config.caseSensitive,
+                   config.bundling,
+                   "backend|b", &backEnd,
+                   "collector|c", &gcType);
+            args = args[1 .. $];
+        }
+        catch (Exception ex)
+        {
+            logf("Error: Could not parse command line: %s", ex.msg);
+            return 2;
+        }
+
+        if (args.length != 1)
+        {
+            log("Error: Exactly one input module must be given.");
+            return 2;
+        }
+
+        auto file = args[0];
+
+        if (file[0] == '.' && file.length <= moduleFileExtension.length + 1)
+        {
+            logf("Error: Input module '%s' has no name part.", file);
+            return 2;
+        }
+
+        if (extension(file) != moduleFileExtension)
+        {
+            logf("Error: Input module '%s' does not end in '%s'.", file, moduleFileExtension);
+            return 2;
+        }
+
+        FileStream stream;
+
+        try
+        {
+            auto manager = new ModuleManager();
+            manager.attach(intrinsicModule);
+
+            auto reader = new ModuleReader(manager);
+            auto mod = reader.load(file);
+            auto main = mod.entryPoint;
+
+            if (!main)
+            {
+                logf("Error: Module %s has no entry point.");
+                return 1;
+            }
+
+            GarbageCollector gc;
+
+            final switch (gcType)
+            {
+                case GarbageCollectorType.libc:
+                    gc = new LibCGarbageCollector();
+                    break;
+                case GarbageCollectorType.dgc:
+                    gc = new DGarbageCollector();
+                    break;
+                static if (isPOSIX)
+                {
+                    case GarbageCollectorType.boehm:
+                        gc = new BoehmGarbageCollector();
+                        break;
+                }
+            }
+
+            auto engine = new JITEngine(backEnd, gc);
+
+            try
+            {
+                // FIXME: Should be int*.
+                return *cast(ubyte*)engine.execute(main, new NoNullList!RuntimeValue()).data;
+            }
+            catch (ExecutionException ex)
+            {
+                // TODO: Print a stack trace.
+
+                auto rtv = ex.exception;
+
+                clear(ex);
+                GC.free(cast(void*)ex);
+
+                clear(rtv);
+                GC.free(cast(void*)rtv);
+            }
+            finally
+            {
+                engine.terminate();
+                gc.terminate();
+            }
+        }
+        catch (IOException ex)
+        {
+            logf("Error: Could not access '%s': %s", file, ex.msg);
+            return 1;
+        }
+        catch (ReaderException ex)
+        {
+            logf("Error: Could not load '%s': %s", file, ex.msg);
+            return 1;
+        }
+        finally
+        {
+            if (stream)
+                stream.close();
+        }
+
         return 0;
     }
 }
